610680c2c6cd1bcd0bf3b144455b0777fb35a401
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-04-12 22:34:44 +0300

    ✨ feat(app): rendering list stats

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ const App = () => {
         packedHandler={handleItemPacked}
         deletedHandler={handleItemDeleted}
       />
-      <Footer />
+      <Footer items={items} />
     </main>
   );
 };
@@ -137,10 +137,18 @@ const ListItem = ({ item, packedHandler, deletedHandler }) => (
   </li>
 );
 
-const Footer = () => {
+const Footer = ({ items }) => {
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const packedPercent = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      🧳 You have x items on your list, and you've already packed x (x%)
+      {!numPacked
+        ? `Start adding items to your packing list 🧳`
+        : packedPercent === 100
+        ? `You've got everything ready to go! 🎫`
+        : `🧳 You have ${numItems} items on your list, and you've already packed ${numPacked} (${packedPercent}%)`}
     </footer>
   );
 };
