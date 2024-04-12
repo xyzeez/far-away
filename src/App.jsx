d97401061593e46d60111d70bac4f04e61d7921a
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-04-12 06:29:14 +0300

    🎉 start(app): initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './index.css';
+
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: false },
+];
+
+const App = () => {};
+
+export default App;
